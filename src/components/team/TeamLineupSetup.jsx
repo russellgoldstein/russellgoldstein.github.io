e73b5c426a6969e8eddaa56e8291989833324637
Russ Goldstein <russell.goldstein@gmail.com>
2024-03-19 08:13:30 -0400

    Fix

--- src/components/team/TeamLineupSetup.jsx
+++ src/components/team/TeamLineupSetup.jsx
@@ -4,16 +4,26 @@ import { Baseball } from '../icons/Baseball';
 import { TeamLineupTable } from './TeamLineupTable';
 import TeamList from './TeamList';
 import { useGetLineupQuery, usePostLineupMutation } from '../../services/gameApi';
+import { useEffect } from 'react';
+import { setTeamHitters } from '../../store/teamSlice';
 
 export default function TeamLineupSetup({ gameCode }) {
   const lineup = useSelector((state) => state.teams.teams['away'].hitters);
-  const { data: savedLineup, error: lineupError, isLoading: lineupIsLoading } = useGetLineupQuery(gameCode);
+  const { data: savedLineup, error: lineupError, isLoading: lineupIsLoading } = useGetLineupQuery({ gameCode });
+  console.log('savedLineup', savedLineup);
   const [postLineup, { error, isLoading }] = usePostLineupMutation();
+
+  useEffect(() => {
+    if (savedLineup) {
+      console.log('savedLineup', savedLineup);
+      setTeamHitters({ teamType: 'away', hitters: savedLineup });
+    }
+  }, [savedLineup]);
 
   const submitLineup = async () => {
     const body = {
       teamType: 'away',
-      lineup: lineup.map((hitter) => hitter.id),
+      lineup: lineup.map((hitter) => hitter.player_id),
     };
     await postLineup({ gameCode, body });
   };
