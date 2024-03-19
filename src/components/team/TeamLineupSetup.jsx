29b6e2ef4484f35cfda8946dc75c72986ce46113
Russ Goldstein <russell.goldstein@gmail.com>
2024-03-19 18:12:46 -0400

    Fixes

--- src/components/team/TeamLineupSetup.jsx
+++ src/components/team/TeamLineupSetup.jsx
@@ -5,26 +5,29 @@ import { TeamLineupTable } from './TeamLineupTable';
 import TeamList from './TeamList';
 import { useGetLineupQuery, usePostLineupMutation } from '../../services/gameApi';
 import { useEffect } from 'react';
-import { setTeamHitters } from '../../store/teamSlice';
+import { setTeamHitters, setTeamPitchers } from '../../store/teamSlice';
 
 export default function TeamLineupSetup({ gameCode }) {
-  const lineup = useSelector((state) => state.teams.teams['away'].hitters);
+  const hittingLineup = useSelector((state) => state.teams.teams['away'].hitters);
+  const pitchingLineup = useSelector((state) => state.teams.teams['away'].pitchers);
   const { data: savedLineup, error: lineupError, isLoading: lineupIsLoading } = useGetLineupQuery({ gameCode });
   const dispatch = useDispatch();
-  console.log('savedLineup', savedLineup);
   const [postLineup, { error, isLoading }] = usePostLineupMutation();
 
   useEffect(() => {
     if (savedLineup) {
-      console.log('savedLineup', savedLineup);
-      dispatch(setTeamHitters({ teamType: 'away', hitters: savedLineup }));
+      dispatch(setTeamHitters({ teamType: 'away', hitters: savedLineup.hitters }));
+      dispatch(setTeamPitchers({ teamType: 'away', pitchers: savedLineup.pitchers }));
     }
-  }, [savedLineup]);
+  }, [savedLineup, dispatch]);
 
   const submitLineup = async () => {
     const body = {
       teamType: 'away',
-      lineup: lineup.map((hitter) => hitter.player_id),
+      lineup: {
+        hitters: hittingLineup.map((hitter) => hitter.player_id),
+        pitchers: pitchingLineup.map((pitcher) => pitcher.player_id),
+      },
     };
     await postLineup({ gameCode, body });
   };
--- src/store/teamSlice.js
+++ src/store/teamSlice.js
@@ -18,8 +18,6 @@ export const teamsSlice = createSlice({
   reducers: {
     setTeamHitters: (state, action) => {
       const { teamType, hitters } = action.payload;
-      console.log('teamType', teamType);
-      console.log('hitters', hitters);
       state.teams[teamType].hitters = hitters;
     },
     setTeamPitchers: (state, action) => {
